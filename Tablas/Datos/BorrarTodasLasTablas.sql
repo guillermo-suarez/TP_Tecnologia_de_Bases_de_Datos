36cc26d60b9a8ad0e6e40ad2a6b3f3d15ab258b7
Guillermo Suarez <40548346+guillermo-suarez@users.noreply.github.com>
2022-05-14 16:16:00 -0300

    Create BorrarTodasLasTablas.sql

---
+++ Tablas/Datos/BorrarTodasLasTablas.sql
@@ -0,0 +1,23 @@
+drop table CERTIOBRA cascade constraints;
+
+drop table CERTIPAGO cascade constraints;
+
+drop table CONCEPTO cascade constraints;
+
+drop table CONCEPTOSXCERTIF cascade constraints;
+
+drop table CONCEPTOSXOBRA cascade constraints;
+
+drop table EMPRESA cascade constraints;
+
+drop table FOJA cascade constraints;
+
+drop table FOJADET cascade constraints;
+
+drop table ITEM cascade constraints;
+
+drop table ITEMCOSTO cascade constraints;
+
+drop table LOCALIDAD cascade constraints;
+
+drop table OBRA cascade constraints;
