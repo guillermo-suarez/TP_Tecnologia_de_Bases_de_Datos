e0f6db460044fc04b5c7ff1dc05daec138d10b21
solmala <92184025+solmala@users.noreply.github.com>
2022-05-15 18:54:22 -0300

    Add files via upload

---
+++ Secuencias/SEQ_ID_FOJA.sql
@@ -0,0 +1,7 @@
+-- Secuencia para ID de FOJA autoincremental
+create sequence seq_id_foja
+minvalue 0
+maxvalue 999999999
+start with 0
+increment by 1
+nocache;
---
+++ Secuencias/SEQ_ID_REDETERMINACION.sql
@@ -0,0 +1,7 @@
+-- Secuencia para ID de REDETERMINACION autoincremental
+create sequence seq_id_redeterminacion
+minvalue 0
+maxvalue 999999999
+start with 1
+increment by 1
+nocache;
