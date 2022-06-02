d45a4b426b8e5b6d3baee2085f9a39561aa445ef
Guillermo Suarez <40548346+guillermo-suarez@users.noreply.github.com>
2022-06-02 16:14:31 -0300

    Actualizar 11-Redeterminacion.sql
    Uno de los insert tenía un dato mal escrito, corregido.

--- Tablas/Datos/11-Redeterminacion.sql
+++ Tablas/Datos/11-Redeterminacion.sql
@@ -1,6 +1,6 @@
 insert into  REDETERMINACION (idredeterminacion, fecharedet, fechadesde, fechahasta, nroresolucion)
 values (1, to_date('25-04-2021', 'dd-mm-yyyy'), to_date('01-05-2021', 'dd-mm-yyyy'), to_date('30-11-2021', 'dd-mm-yyyy'), '2548/2021');
 insert into  REDETERMINACION (idredeterminacion, fecharedet, fechadesde, fechahasta, nroresolucion)
-values (2, to_date('25-11-2021', 'dd-mm-yyyy'), to_date('01-12-2021', 'dd-mm-yyyy'), to_date('31-05-2021', 'dd-mm-yyyy'), '1548/2022');
+values (2, to_date('25-11-2021', 'dd-mm-yyyy'), to_date('01-12-2021', 'dd-mm-yyyy'), to_date('31-05-2022', 'dd-mm-yyyy'), '1548/2022');
 insert into  REDETERMINACION (idredeterminacion, fecharedet, fechadesde, fechahasta, nroresolucion)
 values (3, to_date('26-04-2022', 'dd-mm-yyyy'), to_date('01-06-2022', 'dd-mm-yyyy'), null, '2785/2022');
